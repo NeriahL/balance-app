ee6c76d804804421ef7f94eb1b98c9e66f3998c7
Neriah Levin <neria@Yechiel-Laptop>
2023-01-24 15:10:23 +0200

    Added header file for helper_functions
    helper_functions.h

---
+++ helper_functions.h
@@ -0,0 +1,45 @@
+#ifndef _HELPER_FUNCTIONS_H_
+#define _HELPER_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct Date
+{
+    int month;
+	int day;
+	int year;
+} Date;
+
+typedef struct Customer
+{
+	char first_name[11];
+	char last_name[11];
+	char id[265];
+	char phone_number[11];
+	Date date;
+	int debt;
+
+	struct Customer *next;
+} Customer;
+
+Customer *compare_date(Customer *c1, Customer *c2);
+
+Customer *compare_debt(Customer *c1, Customer *c2);
+
+Customer *compare_first_name(Customer *c1, Customer *c2);
+
+Customer *compare_last_name(Customer *c1, Customer *c2);
+
+Customer *compare_id(Customer *c1, Customer *c2);
+
+Customer *compare_phone_number(Customer *c1, Customer *c2);
+
+int validate_id(Customer *customer, Customer *compare);
+
+void p_cust(Customer *cust);
+
+char *clean(char *str);
+#endif
